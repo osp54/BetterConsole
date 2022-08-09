42b1d7dd41cbe2d75b492ccd62a0a282e94f156f
osp54 <clashmekers54@gmail.com>
2022-08-09 14:41:51 +0300

    Using reflection

--- src/osp/Main.java
+++ src/osp/Main.java
@@ -1,11 +1,9 @@
 package osp;
 
 import arc.Core;
-import arc.files.Fi;
 import arc.util.CommandHandler;
-import arc.util.CommandHandler.*;
 import arc.util.Log;
-import arc.util.Strings;
+import arc.util.Reflect;
 import static arc.util.ColorCodes.*;
 import static arc.util.Log.format;
 import static arc.util.Log.formatColors;
@@ -18,6 +16,8 @@ import org.jline.reader.*;
 import org.jline.terminal.*;
 import org.jline.reader.impl.completer.StringsCompleter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -25,19 +25,27 @@ import java.util.*;
 public class Main extends Plugin{
     private ServerControl serverControl;
     private CommandHandler handler;
-    private Fi currentLogFile;
+    private Method logToFile;
     private Terminal terminal;
     private LineReader lineReader;
-    // анюковские переменные которые приватны в serverControl
-    protected static String[] tags = {"&lc&fb[D]&fr", "&lb&fb[I]&fr", "&ly&fb[W]&fr", "&lr&fb[E]", ""};
-    protected static DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-    public final Fi logFolder = Core.settings.getDataDirectory().child("logs/");
-    private static final int maxLogLength = 1024 * 1024 * 5;
+    private String[] tags;
+    private DateTimeFormatter dateTime;
     @Override
     public void init() {
         // получаем серверКонтрол и хандлер комманд(если создать новый ничего не получится)
         serverControl = (ServerControl) Core.app.getListeners().find(listener -> listener instanceof ServerControl);
         handler = serverControl.handler;
+
+        // получаем приватные переменные из ServerControl
+        try {
+            logToFile = serverControl.getClass().getDeclaredMethod("logToFile", String.class);
+            logToFile.setAccessible(true);
+
+            dateTime = Reflect.get(ServerControl.class, "dateTime");
+            tags = Reflect.get(ServerControl.class, "tags");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
         // помещаем все имена команд в список для автодополнения команды через tab(работает костыльно, нужно доработать)
         List<String> cmds = new ArrayList();
@@ -61,7 +69,8 @@ public class Main extends Plugin{
             //err has red text instead of reset.
             if(level1 == Log.LogLevel.err) text = text.replace(reset, lightRed + bold);
 
-            String result = bold + lightBlack + "[" + dateTime.format(LocalDateTime.now()) + "] " + reset + format(tags[level1.ordinal()] + " " + text + "&fr");
+            String result = null;
+            result = bold + lightBlack + "[" + dateTime.format(LocalDateTime.now()) + "] " + reset + format(tags[level1.ordinal()] + " " + text + "&fr");
 
             if(lineReader.isReading()){
                 lineReader.callWidget(LineReader.CLEAR);
@@ -73,7 +82,11 @@ public class Main extends Plugin{
             }
 
             if(Administration.Config.logging.bool()){
-                logToFile("[" + dateTime.format(LocalDateTime.now()) + "] " + formatColors(tags[level1.ordinal()] + " " + text + "&fr", false));
+                try {
+                    logToFile.invoke(serverControl,"[" + dateTime.format(LocalDateTime.now()) + "] " + formatColors(tags[level1.ordinal()] + " " + text + "&fr", false));
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
@@ -82,12 +95,8 @@ public class Main extends Plugin{
                 try {
                     String line = lineReader.readLine("> ");
                     if(!line.isEmpty()){
-                        Core.app.post(() -> handleCommandString(line));
+                        Core.app.post(() -> serverControl.handleCommandString(line));
                     }
-                }catch (EndOfFileException e){
-                    Core.app.exit();
-                }catch (UserInterruptException e){
-                    Core.app.exit();
                 }catch (Exception e){
                     Log.err(e);
                     Core.app.exit();
@@ -96,52 +105,4 @@ public class Main extends Plugin{
         };
 
     }
-    // анюковские методы которые нельзя получить из ServerControl т.к они приватны
-    public void handleCommandString(String line){
-        CommandHandler handler = serverControl.handler;
-        CommandResponse response = handler.handleMessage(line);
-        if(response.type == ResponseType.unknownCommand){
-            int minDst = 0;
-            Command closest = null;
-
-            for(Command command : handler.getCommandList()){
-                int dst = Strings.levenshtein(command.text, response.runCommand);
-                if(dst < 3 && (closest == null || dst < minDst)){
-                    minDst = dst;
-                    closest = command;
-                }
-            }
-
-            if(closest != null && !closest.text.equals("yes")){
-                Log.err("Command not found. Did you mean \"" + closest.text + "\"?");
-            }else{
-                Log.err("Invalid command. Type 'help' for help.");
-            }
-        }else if(response.type == ResponseType.fewArguments){
-            Log.err("Too few command arguments. Usage: " + response.command.text + " " + response.command.paramText);
-        }else if(response.type == ResponseType.manyArguments){
-            Log.err("Too many command arguments. Usage: " + response.command.text + " " + response.command.paramText);}
-    }
-    private void logToFile(String text){
-        if(currentLogFile != null && currentLogFile.length() > maxLogLength){
-            currentLogFile.writeString("[End of log file. Date: " + dateTime.format(LocalDateTime.now()) + "]\n", true);
-            currentLogFile = null;
-        }
-
-        for(String value : values){
-            text = text.replace(value, "");
-        }
-
-        if(currentLogFile == null){
-            int i = 0;
-            while(logFolder.child("log-" + i + ".txt").length() >= maxLogLength){
-                i++;
-            }
-
-            currentLogFile = logFolder.child("log-" + i + ".txt");
-        }
-
-        currentLogFile.writeString(text + "\n", true);
-    }
-
 }
